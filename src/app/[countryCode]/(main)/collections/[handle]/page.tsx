ca3e091a3ee988544abd2f892ea62dde4ce114fe
Yoyo99 <lauboin@free.fr>
2025-12-09 18:43:18 +0100

    Remove static parameter generation logic
    Disable static parameter generation for collections.

--- src/app/[countryCode]/(main)/collections/[handle]/page.tsx
+++ src/app/[countryCode]/(main)/collections/[handle]/page.tsx
@@ -18,36 +18,7 @@ type Props = {
 export const PRODUCT_LIMIT = 12
 
 export async function generateStaticParams() {
-  const { collections } = await listCollections({
-    fields: "*products",
-  })
-
-  if (!collections) {
-    return []
-  }
-
-  const countryCodes = await listRegions().then(
-    (regions: StoreRegion[]) =>
-      regions
-        ?.map((r) => r.countries?.map((c) => c.iso_2))
-        .flat()
-        .filter(Boolean) as string[]
-  )
-
-  const collectionHandles = collections.map(
-    (collection: StoreCollection) => collection.handle
-  )
-
-  const staticParams = countryCodes
-    ?.map((countryCode: string) =>
-      collectionHandles.map((handle: string | undefined) => ({
-        countryCode,
-        handle,
-      }))
-    )
-    .flat()
-
-  return staticParams
+  return [] // Désactive la génération statique
 }
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
