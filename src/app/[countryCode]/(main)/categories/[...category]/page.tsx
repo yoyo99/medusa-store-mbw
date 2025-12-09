be7b336b3df1037e094db48c4933c1ec64021520
Yoyo99 <lauboin@free.fr>
2025-12-09 18:42:51 +0100

    Remove static params generation logic
    Disable static parameter generation for categories.

--- src/app/[countryCode]/(main)/categories/[...category]/page.tsx
+++ src/app/[countryCode]/(main)/categories/[...category]/page.tsx
@@ -16,30 +16,7 @@ type Props = {
 }
 
 export async function generateStaticParams() {
-  const product_categories = await listCategories()
-
-  if (!product_categories) {
-    return []
-  }
-
-  const countryCodes = await listRegions().then((regions: StoreRegion[]) =>
-    regions?.map((r) => r.countries?.map((c) => c.iso_2)).flat()
-  )
-
-  const categoryHandles = product_categories.map(
-    (category: any) => category.handle
-  )
-
-  const staticParams = countryCodes
-    ?.map((countryCode: string | undefined) =>
-      categoryHandles.map((handle: any) => ({
-        countryCode,
-        category: [handle],
-      }))
-    )
-    .flat()
-
-  return staticParams
+  return [] // Désactive la génération statique
 }
 
 export async function generateMetadata(props: Props): Promise<Metadata> {
