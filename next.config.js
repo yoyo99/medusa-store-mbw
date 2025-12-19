6ddee28db2c396395e222773d4a936ac64586290
Yoyo99 <lauboin@free.fr>
2025-12-19 18:20:25 +0100

    Set output to 'standalone' in next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,7 @@ const S3_PATHNAME = process.env.MEDUSA_CLOUD_S3_PATHNAME
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  output: 'standalone',
   reactStrictMode: true,
   logging: {
     fetches: {
